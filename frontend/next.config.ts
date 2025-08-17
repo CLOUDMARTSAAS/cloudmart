ced91866cb1b3dabcaa3f06ed66a1d5004dcceb3
pks9862728888 <pks9862728888@gmail.com>
2025-08-17 16:11:44 +0530

    Created docker file for next build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* comment out the below for local */
-  // output: 'export',
-  // trailingSlash: false,
-  // images: {
-  //   unoptimized: true,
-  // },
-  // distDir: '../public'
 };
 
 export default nextConfig;
